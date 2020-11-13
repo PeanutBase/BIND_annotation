74295fc342aed4d04d59467bbae7194455a84f20
Jacqueline Campbell <jdjax@iastate.edu>
2020-11-13 16:19:07 -0600

    Update 00.EST_trimming.sh

--- scripts/prepare_ESTs/00.EST_trimming.sh
+++ scripts/prepare_ESTs/00.EST_trimming.sh
@@ -4,5 +4,4 @@ module load fastx/0.0.14
 ## convert fastq to fasta and more N's
 fastq_to_fasta -r -v -i species.ESTs.fastq -o species.ESTs.fasta
 
-## convert fastq to fasta
-fastq_to_fasta -r -n -v -i species.ESTs.fastq -o species.ESTs.KeepNs.fasta
+
