ccb87cb6fa7e61a42e0f179acbc7a58ae33fb734
Jacqueline Campbell <jdjax@iastate.edu>
2020-11-13 16:36:44 -0600

    Update 00.trim_cluster.sh

--- scripts/prepare_ESTs/00.trim_cluster.sh
+++ scripts/prepare_ESTs/00.trim_cluster.sh
@@ -11,3 +11,9 @@ module load cap3/3
 cap3 species.ESTs.fasta
 
 
+## cap3 output generated *clustered and *singlets fasta files
+## Before aligning the ESTs sequences to the genome:
+### 1. Combine *clustered and *singlets fasta files
+### 2. Remove any ESTs sequences that are less than 200 bp
+
+
