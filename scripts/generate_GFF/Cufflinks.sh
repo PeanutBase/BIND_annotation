246e8d01ce1fcdf6de0531998b6a025da98c8b01
Jacqueline Campbell <jdjax@iastate.edu>
2020-11-14 17:35:26 -0600

    Update Cufflinks.sh

--- scripts/generate_GFF/Cufflinks.sh
+++ scripts/generate_GFF/Cufflinks.sh
@@ -2,6 +2,6 @@
 module load  cufflinks/2.2.1
 
 out="/project/peanut_annotation/BIND_annotations/03.cufflinks/"
-bam=".Aligned.sorted.bam"
+bam="genome_merged.Aligned.sorted.bam"
 
 cufflinks --output-dir $out --num-threads 20 --frag-len-mean 100 --verbose --no-update-check $bam
