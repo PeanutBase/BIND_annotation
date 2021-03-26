c81914b11c4a60cb0ab761912c9eedc2c375037e
Jacqueline Campbell <jdjax@iastate.edu>
2021-03-26 11:49:03 -0500

    Delete Trinity.sh
    No longer needed for this work.

--- scripts/generate_GFF/Trinity.sh
+++
@@ -1,6 +0,0 @@
-module load trinityrnaseq/2.10.0
-
-bam=$1
-out=$2
-
-Trinity --genome_guided_bam $bam --max_memory 750G --min_contig_length 100 --genome_guided_max_intron 10000 --full_cleanup --CPU 20 --output TrinityOut_${out} 
