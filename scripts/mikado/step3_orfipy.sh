0c6fb3070d24f6180445992e7a33b1b15a8903b0
Jacqueline Campbell <jdjax@iastate.edu>
2021-04-16 15:12:11 -0500

    Update step3_orfipy.sh

--- scripts/mikado/step3_orfipy.sh
+++ scripts/mikado/step3_orfipy.sh
@@ -1,6 +1,10 @@
 #paper: https://academic.oup.com/bioinformatics/advance-article/doi/10.1093/bioinformatics/btab090/6134074
 #conda package: https://anaconda.org/bioconda/orfipy
 
+ml miniconda
+
+source activate orfipy
+
 orfipy DI_prepared.fasta \
   --outdir orfipy_out  \
   --bed12 orfs.bed \
