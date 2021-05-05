3a3d82254bdc119e143564960023e43f34cae0c6
Jacqueline Campbell <jdjax@iastate.edu>
2021-05-05 13:13:15 -0500

    Update runRepeatmasker.sh

--- scripts/braker/runRepeatmasker.sh
+++ scripts/braker/runRepeatmasker.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
-source /work/LAS/mhufford-lab/arnstrm/miniconda/etc/profile.d/conda.sh
+
 conda activate repeatmasker
+
 lib="$1"
-genome=$2
+genome="$2"
+
 RepeatMasker \
   -e ncbi \
   -pa 36 \
