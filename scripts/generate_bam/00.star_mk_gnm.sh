a677c96f1b1c94d00860ae9cf400ae3b36c224de
Jacqueline Campbell <jd.campbell.phd@gmail.com>
2026-04-03 10:53:18 -0500

    Update STAR module version and script parameters

--- scripts/generate_bam/00.star_mk_gnm.sh
+++ scripts/generate_bam/00.star_mk_gnm.sh
@@ -1,6 +1,12 @@
-module load star/2.7.2b
+module load star/2.7.11b
+# Input Agruements
+OUT="$1"
+GNM="$2"
+GTF="$3"
 
-$out="/project/peanut/annotations/01_genome.star" ## Location for output
-$gmn=$1   ## genome fasta file 
-
-STAR --runMode genomeGenerate --runThreadN 20 --genomeDir $out --genomeFastaFiles $gmn
+STAR \
+--runThreadN 10 \
+--runMode genomeGenerate \
+--genomeDir /project/Mikado_transcriptome/02.star.genomes/"$OUT" \
+--genomeFastaFiles "$GNM" \
+--sjdbGTFfile "$GTF"
