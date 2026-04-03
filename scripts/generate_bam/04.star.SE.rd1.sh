d9f4876fd045b1ef9de9d7f0ef42985735a395d8
Jacqueline Campbell <jd.campbell.phd@gmail.com>
2026-04-03 11:04:24 -0500

    Refactor STAR script for new genome directory

--- scripts/generate_bam/04.star.SE.rd1.sh
+++ scripts/generate_bam/04.star.SE.rd1.sh
@@ -1,11 +1,18 @@
-  
-module load star/2.7.2b 
 
+module load star/2.7.11b
+
+# Input Agruements
 index="/project/peanut/annotations/01_genome.star/"
 read="species.SE.fastq.gz"
 out="species.SE.rd1."
 
-STAR --genomeDir $index --runThreadN 20 --runMode alignReads --readFilesIn $read --readFilesCommand zcat --outFileNamePrefix $out --outSAMtype None
+STAR \
+--genomeDir $index \
+--runThreadN 20 \
+--readFilesIn $read 
+--readFilesCommand zcat \
+--outFileNamePrefix $out 
+--outSAMtype None
 
 ## slurm job trick: You can submit star.SE.rd2.sh right after star.SE.rd1.sh by using "sbatch --dependency=afterok:" 
 ### EXAMPLE: sbatch --dependency=afterok:5136041 star.SE.rd2.sh (where '5136041' is star.SE.rd1.sh job number
