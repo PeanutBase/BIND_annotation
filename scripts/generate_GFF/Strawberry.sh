a25c4f40c896eda018890bb741701f3898c79590
Jacqueline Campbell <jd.campbell.phd@gmail.com>
2026-04-08 14:24:13 -0500

    Update Strawberry.sh

--- scripts/generate_GFF/Strawberry.sh
+++ scripts/generate_GFF/Strawberry.sh
@@ -1,7 +1,10 @@
-# Strawberry was downloaded/installed using singularity
+ml apptainer
 
-bam=$1
-out=$2
 
-singularity exec --cleanenv strawberry_v1.1.1.sif /home/strawberry/bin/strawberry --output-gtf ${out}_strawberry.gtf \
---logfile ${out}_strawberry_assembled.log --no-quant --num-threads 20 --verbose --min-transcript-size 100 ${bam}
+BAM="$1"
+OUT="$2"
+
+apptainer exec --cleanenv /project/legume_project/containers/strawberry_v1.1.1.sif \
+  /home/strawberry/bin/strawberry --output-gtf "$OUT"_strawberry.gtf \
+  --logfile "$OUT"_strawberry_assembled.log --no-quant --num-threads 10 \
+  --verbose --min-transcript-size 100 "$BAM"
